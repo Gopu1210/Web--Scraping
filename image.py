64c67a04de2e0820e1bdadde200f9798c0a9ecaa
gopika narayanan <63047157+Gopu1210@users.noreply.github.com>
2020-04-10 20:08:01 +0530

    Add files via upload

---
+++ image.py
@@ -0,0 +1,36 @@
+import HTMLParser
+from urllib.request import urlopen
+import sys
+
+urlString = "https://autoportal.com/upcoming-cars/"
+
+def getImage(addr):
+    u = urlopen(addr)
+    data = u.read()
+
+    splitPath = addr.split('/')
+    fName = splitPath.pop()
+    print (fName)
+
+    f = open(fName, 'wb')
+    f.write(data)
+    f.close()
+
+class parseImages(HTMLParser.HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        if tag == 'img':
+            for name,value in attrs:
+                if name == 'src':
+                    getImage(urlString + "/" + value)
+
+lParser = parseImages()
+
+u = urlopen(urlString)
+print (u.info())
+
+lParser.feed(u.read())
+lParser.close()
+
+
+
+
